dbe1e84900038e9cd84c40071dbd5a2c05dada58
hkidd <harrisonakidd@gmail.com>
2021-10-19 12:13:28 -0500

    URL's are being shortened

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useUnifiedTopology: true
+});
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+});
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullURL })
+
+    res.redirect('/')
+});
 
 app.listen(process.env.PORT || 5000);
-
-app.get('/', function (req, res) {
-    res.render('index')
-})
